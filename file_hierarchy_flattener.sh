55d7de2ddbf05ceb92abdda983e592a73850447b
xoonel <44796822+xoonel@users.noreply.github.com>
2018-11-08 17:58:59 +0100

    Create file_hierarchy_flattener.sh

---
+++ file_hierarchy_flattener.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#Need to add var to define user input FROM path
+#Need to add var to define user input TO path
+
+#Does as expected. Before uncommenting and running, make sure to define paths. Will work on user input prompts for FROM and TO paths.  
+#find ~/From_Path/Subpath '*.txt' -exec cp {} ~/To_Path/Subpath \;
