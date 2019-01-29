58389307e48442618d035a6cc0e7cbc7bb226d13
xoonel <44796822+xoonel@users.noreply.github.com>
2019-01-29 15:22:16 +0100

    Update file_hierarchy_flattener.sh

--- file_hierarchy_flattener.sh
+++ file_hierarchy_flattener.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-#Need to add var to define user input FROM path
-#Need to add var to define user input TO path
+#Flatten-the-Hierachy
 
-#Does as expected. Before uncommenting and running, make sure to define paths. Will work on user input prompts for FROM and TO paths.  
-#find ~/From_Path/Subpath '*.txt' -exec cp {} ~/To_Path/Subpath \;
+# Make sure to make the shell script executable before attempting to run.  
+#chmod a+x [~/Path/Subpath/Scriptname.sh]
+
+#Gets user input for file path to copy from. Note: For now it must be the full path
+echo Enter the file path you wish to copy from:  
+read varnameFROM
+
+#Gets user input for file path to copy to. Note: For now it must be the full path
+echo Enter the file path you wish to copy to:  
+read varnameTO
+
+#Need to add a count function for number of files of directory levels (pre-counted before executing line 14).
+echo Copied XX files from $varnameFROM to $varnameTO and removed XX directory levels.
+
+find $varnameFROM -exec cp {} $varnameTO \;
+
+#Future feature to exclude/include various file types
